79626943a68dfce0d457c3146e9575db84fe7fff
Jordan Vera <verawebdevelopment@gmail.com>
2024-06-02 19:50:43 -0500

    chore: Update package.json scripts and dependencies

---
+++ src/scripts/getData.js
@@ -0,0 +1,51 @@
+import 'dotenv/config';
+
+export const fetchAllTeamsAndReturnTeamIds = async () => {
+  const response = await fetch(
+    `https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=${process.env.SPORTSDATAIO_API_KEY}`,
+    {
+      method: 'GET',
+    }
+  );
+
+  const json = await response.json();
+
+  // console.log({ json });
+
+  const teamIds = json.map((team) => team.TeamID);
+
+  console.log(teamIds);
+
+  return teamIds;
+};
+
+export const fetchSeasonGameData = async (season, teamIds) => {
+  const resultsForEachTeamBySeason = await Promise.all(
+    teamIds.map(async (teamId) => {
+      const response = await fetch(
+        `https://api.sportsdata.io/v3/nfl/scores/json/TeamGameStatsBySeason/${season}/${teamId}/all?key=${process.env.SPORTSDATAIO_API_KEY}`,
+        {
+          method: 'GET',
+        }
+      );
+
+      return response.json();
+    })
+  );
+
+  console.log({ resultsForEachTeamBySeason });
+  console.log(
+    'length of resultsForEachTeamBySeason: ',
+    resultsForEachTeamBySeason.length
+  );
+
+  return resultsForEachTeamBySeason;
+};
+
+try {
+  const ids = await fetchAllTeamsAndReturnTeamIds();
+
+  await fetchSeasonGameData(2023, ids);
+} catch (error) {
+  console.log(error);
+}
