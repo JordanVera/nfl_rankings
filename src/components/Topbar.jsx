15cab38ae5dae1479d424113eaf119f944c3176e
Jordan Vera <verawebdevelopment@gmail.com>
2024-06-11 00:51:20 -0500

    chore: Update Topbar component to include logo and improve navigation links

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,21 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Topbar = () => {
   return (
     <header className="border-b border-gray-800 p-5">
-      <div className="flex gap-5">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+      <div className="max-w-[1200px] mx-auto">
+        <div className="flex items-center gap-2">
+          <Link href="/">
+            <Image src="/media/logoWhite.svg" height={30} width={30} />
+          </Link>
+          <Link href="/" className="hover:bg-gray-800 rounded-md p-2">
+            Home
+          </Link>
+          <Link href="/about" className="hover:bg-gray-800 rounded-md p-2">
+            About
+          </Link>
+        </div>
       </div>
     </header>
   );
